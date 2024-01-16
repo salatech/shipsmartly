be030fd3b8f2038252b67dcdd259e64cde43a1c1
salatech <solahudeenbabatunde@gmail.com>
2024-01-16 13:21:37 +0100

    fix: update the admin url key

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,56 @@
-import React, { useEffect } from 'react';
-import {HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Home from './pages/Home';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import About from './pages/About';
-import Track from './pages/Track';
-import Dashboard from './pages/Dashboard';
-import ContactUs from './pages/ContactUs';
+import React, { useEffect } from "react";
+import {
+	HashRouter as Router,
+	Route,
+	Routes,
+	Navigate,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import About from "./pages/About";
+import Track from "./pages/Track";
+import Dashboard from "./pages/Dashboard";
+import ContactUs from "./pages/ContactUs";
 
 function App() {
-  // Use useEffect to trigger the redirection
-  useEffect(() => {
-    if (window.location.href.endsWith('/##admin')) {
-      // Redirect to an external URL
-      window.location.href = 'https://api.shipsmartlyservices.com/admin';
-    }
-  }, []);
+	// Use useEffect to trigger the redirection
+	useEffect(() => {
+		if (window.location.href.endsWith("/##admin")) {
+			// Redirect to an external URL
+			window.location.href = "https://api-shipsmartlyservices.com";
+		}
+	}, []);
 
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/Track" element={<Track />} />
-        <Route path="/contact" element={<ContactUs />} />
-        <Route path="/Dashboard/:code" element={<Dashboard />} />
-        {/* No need for a route for /admin */}
-      </Routes>
-      <Footer />
-    </Router>
-  );
+	return (
+		<Router>
+			<Navbar />
+			<Routes>
+				<Route
+					path="/"
+					element={<Home />}
+				/>
+				<Route
+					path="/About"
+					element={<About />}
+				/>
+				<Route
+					path="/Track"
+					element={<Track />}
+				/>
+				<Route
+					path="/contact"
+					element={<ContactUs />}
+				/>
+				<Route
+					path="/Dashboard/:code"
+					element={<Dashboard />}
+				/>
+				{/* No need for a route for /admin */}
+			</Routes>
+			<Footer />
+		</Router>
+	);
 }
 
 export default App;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -24,7 +24,7 @@ const Dashboard = () => {
 
         if (response.data.status === "success") {
           setTrackingData(response.data.data);
-          setError(null); // Clear any previous errors
+          setError(null); // Clear any previous error
         } else {
           setError(
             "Tracking Order does not exist, please check your tracking code again"
