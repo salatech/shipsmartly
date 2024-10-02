edd242b31bdef4abe7210553cea26d5ab789ef1f
salatech <solahudeenbabatunde@gmail.com>
2024-10-02 20:49:24 +0100

    feat: update admin endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
 	useEffect(() => {
 		if (window.location.href.endsWith("/##admin")) {
 			// Redirect to an external URL
-			window.location.href = "https://api.shipsmartlyservice.com";
+			window.location.href = "https://api.shipsmartlyservice.com/admin";
 		}
 	}, []);
 
